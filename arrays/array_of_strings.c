0293f3c23fa7e9f9613167302f4963f8e3ccc623
Stephen Stack <Stephen.Stack@digi.com>
2015-05-19 11:48:12 -0500

    arrays: update

--- arrays/array_of_strings.c
+++ arrays/array_of_strings.c
@@ -1,11 +1,15 @@
 #include <stdio.h>
 
-int main(int argc, char *argv[])
-{
-    // array of strings
+int main(int argc, char *argv[]) {
+
+    // array of strings (Pointer of pointers)j
+    // Each element in the <states> array is a pointer which is 64 or 32 bits on
+    // a typical desktop
     char *states[] = {
-            "California", "Oregon",
-            "Washington", "Texas"
+            "California",
+            "Oregon",
+            "Washington",
+            "Texas"
     };
 
     char *c_style_string = "c style string";  // automatically null terminates
@@ -18,4 +22,6 @@ int main(int argc, char *argv[])
     // This doesn't print correctly because there is no such thing as an
     // "array of strings" it's just a 2 dimensional array of characters
     printf("%s\n", states);
+
+    return 0;
 }
